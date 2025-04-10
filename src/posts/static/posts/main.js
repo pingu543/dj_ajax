64433a18151a13298e8ee84628d0bc76d6c87b60
pingu543 <pcu50208@outlook.com>
2025-04-10 08:42:05 -0400

    Can add posts.

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -5,6 +5,11 @@ const spinnerBox = document.getElementById('spinner-box')
 const loadBtn = document.getElementById('load-btn')
 const endBox = document.getElementById('end-box')
 
+const postForm = document.getElementById('post-form')
+const title = document.getElementById('id_title')
+const body = document.getElementById('id_body')
+const alertBox = document.getElementById('alert-box')
+
 $.ajaxSetup({
     headers: {
         'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
@@ -109,5 +114,50 @@ loadBtn.addEventListener('click', () => {
     visible += 3
     getData()
 })
+
+postForm.addEventListener('submit', e => {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: '',
+        headers: {
+            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+        },
+        data: {
+            'title': title.value,
+            'body': body.value
+        },
+        success: function(response) {
+            console.log('Post submitted successfully', response);
+            postsBox.insertAdjacentHTML('afterbegin', `
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${response.title}</h5>
+                        <p class="card-text">${response.body}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-2">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                            <form class="like-unlike-forms" data-form-id="${response.id}">
+                                <button type="submit" class="btn btn-primary" id="like-unlike-${response.id}">${response.liked ? `Unlike (${response.count})` : `Like (0)`}</button>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `)
+            likeUnlikePosts()
+            $('#addPostModal').modal('hide')
+            handleAlerts('success', 'Post created successfully')
+        },
+        error: function(error) {
+            console.error('Error submitting post', error);
+            handleAlerts('danger', 'Error submitting post')
+        }
+    });
+});
 
 getData()
--- src/posts/views.py
+++ src/posts/views.py
@@ -13,6 +13,12 @@ def post_list_and_create(request):
             instance = form.save(commit=False)
             instance.author = author
             instance.save()
+            return JsonResponse({
+                'id': instance.id,
+                'title': instance.title,
+                'body': instance.body,
+                'author': instance.author.user.username,
+            })
     # qs = Post.objects.all()
     context = {
         'form': form,
@@ -20,24 +26,25 @@ def post_list_and_create(request):
     return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
-    visible = 3
-    upper = num_posts
-    lower = upper - visible
-    size = Post.objects.all().count()
-    
-    qs = Post.objects.all()
-    data = []
-    for obj in qs:
-        item = {
-            'id' : obj.id,
-            'title' : obj.title,
-            'body' : obj.body,
-            'liked' : True if request.user in obj.liked.all() else False,
-            'count' : obj.like_count,
-            'author' : obj.author.user.username,
-        }
-        data.append(item)
-    return JsonResponse({'data': data[lower:upper], 'size': size})
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        visible = 3
+        upper = num_posts
+        lower = upper - visible
+        size = Post.objects.all().count()
+        
+        qs = Post.objects.all()
+        data = []
+        for obj in qs:
+            item = {
+                'id' : obj.id,
+                'title' : obj.title,
+                'body' : obj.body,
+                'liked' : True if request.user in obj.liked.all() else False,
+                'count' : obj.like_count,
+                'author' : obj.author.user.username,
+            }
+            data.append(item)
+        return JsonResponse({'data': data[lower:upper], 'size': size})
 
 def like_unlike_post(request):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
